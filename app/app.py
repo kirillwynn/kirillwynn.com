04c33460b2009cbe0494ae8e625507d051ac40fa
Kirill Wynn <okiedoke@yandex.ru>
2025-03-23 14:32:52 +0300

    feat(app, nginx): add /webhook endpoint and X-Forwarded-Proto header

--- app/app.py
+++ app/app.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello, world! Flask app is running."
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    return "Webhook is working! Flask app is running."
+    
