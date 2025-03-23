6360fa79567a70e4725787e92a3e84a44c6c26fa
Kirill Wynn <okiedoke@yandex.ru>
2025-03-23 16:24:31 +0300

    export vars from GitHub Secrets for correct DB connection

--- app/app.py
+++ app/app.py
@@ -8,9 +8,10 @@ app = Flask(__name__)
 db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
 db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
 db_name = os.getenv("DB_NAME")
 
-DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}/{db_name}"
+DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 engine = create_engine(DATABASE_URL)
 
 @app.route('/')
