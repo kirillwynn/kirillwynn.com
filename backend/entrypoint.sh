48d654143069817c0da8a9ca1479ffb291c0ac74
Kirill Wynn <okiedoke@yandex.ru>
2025-03-30 01:30:38 +0300

    auto migrations after push

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "Running Alembic migrations..."
-alembic upgrade head
+set -e
+
+echo "Setting environment..."
+export FLASK_APP=app
+export FLASK_ENV=production
+
+echo "Running Alembic upgrade (existing migrations)..."
+flask db upgrade
+
+echo "Autogenerating new Alembic migrations if needed..."
+flask db migrate -m "Auto migration" || true
+
+echo "Applying new Alembic migrations..."
+flask db upgrade
 
 echo "Starting Gunicorn..."
 exec gunicorn --bind 0.0.0.0:5000 app.wsgi:app
-
