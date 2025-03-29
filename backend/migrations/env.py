7e4b3bde1d6409f627994e41cb3cd4de56f4c642
Kirill Wynn <okiedoke@yandex.ru>
2025-03-30 02:47:08 +0300

    add with construction at env.py

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -5,56 +5,52 @@ from alembic import context
 from app import create_app
 from app.extensions import db
 
-app = create_app()
-app_context = app.app_context()
-app_context.push()
-
 config = context.config
 fileConfig(config.config_file_name)
 logger = logging.getLogger("alembic.env")
 
-config.set_main_option("sqlalchemy.url", app.config["SQLALCHEMY_DATABASE_URI"])
+app = create_app()
 
-target_metadata = db.metadata
+with app.app_context():
+    config.set_main_option("sqlalchemy.url", app.config["SQLALCHEMY_DATABASE_URI"])
+    target_metadata = db.metadata
 
-def run_migrations_offline():
-    """Run migrations in 'offline' mode."""
-    url = app.config["SQLALCHEMY_DATABASE_URI"]
-    context.configure(
-        url=url,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-    )
-
-    with context.begin_transaction():
-        context.run_migrations()
-
-
-def run_migrations_online():
-    """Run migrations in 'online' mode."""
-
-    def process_revision_directives(context, revision, directives):
-        if getattr(config.cmd_opts, "autogenerate", False):
-            script = directives[0]
-            if script.upgrade_ops.is_empty():
-                directives[:] = []
-                logger.info("No changes in schema detected.")
-
-    connectable = db.engine
-
-    with connectable.connect() as connection:
+    def run_migrations_offline():
+        """Run migrations in 'offline' mode."""
+        url = app.config["SQLALCHEMY_DATABASE_URI"]
         context.configure(
-            connection=connection,
+            url=url,
             target_metadata=target_metadata,
-            process_revision_directives=process_revision_directives,
+            literal_binds=True,
+            dialect_opts={"paramstyle": "named"},
         )
 
         with context.begin_transaction():
             context.run_migrations()
 
+    def run_migrations_online():
+        """Run migrations in 'online' mode."""
 
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    run_migrations_online()
+        def process_revision_directives(context, revision, directives):
+            if getattr(config.cmd_opts, "autogenerate", False):
+                script = directives[0]
+                if script.upgrade_ops.is_empty():
+                    directives[:] = []
+                    logger.info("No changes in schema detected.")
+
+        connectable = db.engine
+
+        with connectable.connect() as connection:
+            context.configure(
+                connection=connection,
+                target_metadata=target_metadata,
+                process_revision_directives=process_revision_directives,
+            )
+
+            with context.begin_transaction():
+                context.run_migrations()
+
+    if context.is_offline_mode():
+        run_migrations_offline()
+    else:
+        run_migrations_online()
