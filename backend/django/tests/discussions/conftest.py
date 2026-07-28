496110eecc92cc6c156b74255844b5ff0796ec45
Kirill Wynn <kirillwynn@Kirills-MacBook-Pro.local>
2026-07-28 12:25:58 -0700

    test: restore Wagtail bootstrap after flush

--- backend/django/tests/discussions/conftest.py
+++ backend/django/tests/discussions/conftest.py
@@ -1,6 +1,7 @@
 import pytest
+from django.conf import settings
 from django.contrib.auth import get_user_model
-from wagtail.models import Page
+from wagtail.models import Locale, Page, Site
 
 from apps.blog.models import BlogIndexPage, BlogPostPage
 
@@ -9,7 +10,13 @@ from apps.blog.models import BlogIndexPage, BlogPostPage
 def blog_index():
     root = Page.get_first_root_node()
     if root is None:
+        Locale.objects.get_or_create(language_code=settings.LANGUAGE_CODE)
         root = Page.add_root(instance=Page(title="Root", slug="root"))
+        Site.objects.create(
+            hostname="localhost",
+            root_page=root,
+            is_default_site=True,
+        )
     index = BlogIndexPage(title="Blog", slug="blog", live=False)
     root.add_child(instance=index)
     return index
